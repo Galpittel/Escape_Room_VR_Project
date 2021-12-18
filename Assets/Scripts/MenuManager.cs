d13f75d0d8be82cdf5089c2486c28ab6450f68af
Ori Silberman <orisil1809@gmail.com>
2021-12-18 11:50:35 +0200

    Added flip button sound and fixed multiple presses on flip

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -16,10 +16,15 @@ public class MenuManager : MonoBehaviour
     public GameObject flip_btn;
     public GameObject leaderBoardTitle;
     public GameObject rankingText;
+    public GameObject gameTitle;
+    private AudioSource flip_sound;
+    public Button button_Leaderboard;
+    public Button button_flipBack;
     // Start is called before the first frame update
     void Start()
     {
         start_menu_animator = gameObject.GetComponent<Animator>();
+        flip_sound = gameObject.GetComponent<AudioSource>();
         //ActivateButtons();
     }
 
@@ -36,7 +41,7 @@ public class MenuManager : MonoBehaviour
     {
         start_menu_animator.SetTrigger("RotateBack");
     }
-    public void DisableButtons() //After First Flip
+    public void DisableFrontButtons() //After First Flip
     {
         //start_btn.SetActive(false);
         //exit_btn.SetActive(false);
@@ -50,20 +55,32 @@ public class MenuManager : MonoBehaviour
         //leaderBoard_btn.enabled = false;
         //flip_btn.enabled = true;
 
-        leaderBoardTitle.SetActive(true);
-        rankingText.SetActive(true);
-        flip_btn.SetActive(true);
-        Invoke("my_Disable", 1.5f);
+        //Previous Solution - 12.12.21 - using fade material
+        //leaderBoardTitle.SetActive(true);
+        //rankingText.SetActive(true);
+        //flip_btn.SetActive(true);
+
+
+        button_Leaderboard.enabled = false;
+        button_flipBack.enabled = true;
+        Invoke("my_Front_Disable", 1.5f);
         
 
     }
-    public void my_Disable()
+    private void my_Front_Disable()
     {
+        //New Change - after removing fade material
+        leaderBoardTitle.SetActive(true);
+        rankingText.SetActive(true);
+        flip_btn.SetActive(true);
+
+        gameTitle.SetActive(false);
         start_btn.SetActive(false);
         exit_btn.SetActive(false);
         leaderBoard_btn.SetActive(false);
+        //
     }
-    public void ActivateButtons() //After Second flip
+    public void ActivateFrontButtons() //After Second flip
     {
         //leaderBoardTitle.SetActive(false);
         //rankingText.SetActive(false);
@@ -72,16 +89,32 @@ public class MenuManager : MonoBehaviour
         //leaderBoard_btn.enabled = true;
         //flip_btn.enabled = false;
 
+
+        //Previous Solution - 12.12.21 - using fade material
+        //start_btn.SetActive(true);
+        //exit_btn.SetActive(true);
+        //leaderBoard_btn.SetActive(true);
+
+        button_flipBack.enabled = false;
+        button_Leaderboard.enabled = true;
+        Invoke("my_Activate_Front", 1.5f);
+    }
+    private void my_Activate_Front()
+    {
+        //New Change - after removing fade material
+        gameTitle.SetActive(true);
         start_btn.SetActive(true);
         exit_btn.SetActive(true);
         leaderBoard_btn.SetActive(true);
-        Invoke("my_Disable2", 1.5f);
-    }
-    public void my_Disable2()
-    {
+
         leaderBoardTitle.SetActive(false);
         rankingText.SetActive(false);
         flip_btn.SetActive(false);
         
+    }
+    public void playFlipSound()
+    {
+        flip_sound.Play();
+        //flip_sound.
     }
 }
--- Assets/Scripts/Scene_Manager.cs
+++ Assets/Scripts/Scene_Manager.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Scene_Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text rankingText;
     void Start()
     {
-        
+
+        Load_RankingData_From_CSV();
     }
 
     // Update is called once per frame
@@ -18,6 +21,29 @@ public class Scene_Manager : MonoBehaviour
         
     }
 
+    private void Load_RankingData_From_CSV()
+    {
+        //int prevUserId = PlayerPrefs.GetInt("UserId", 0); //If no Int of this name exists, the default is 0.
+        //PlayerPrefs.SetInt("UserId", prevUserId + 1);
+        string resulted_ranking = string.Empty;
+        string path = System.IO.Path.GetFullPath("Assets\\OurLogs\\log.csv");
+        StreamReader strReader = new StreamReader(path);
+        bool endOfFile = false;
+        string data_String = string.Empty;
+        while (!endOfFile)
+        {
+            data_String = strReader.ReadLine();
+            if(data_String == null)
+            {
+                endOfFile = true;
+                break;
+            }
+            var data_values = data_String.Split(',');
+            Debug.Log(data_values[0].ToString() + " " + data_values[1].ToString());
+            resulted_ranking += "User " + data_values[0].ToString() + " : " + data_values[1].ToString() + "\r\n";
+        }
+        rankingText.text = resulted_ranking;
+    }
     public void LoadFirstRoom()
     {
         Invoke("LoadRoom", 2);
