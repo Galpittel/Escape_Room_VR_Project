de088136348825a56a7b186424aa0d2dee2df1c0
Ori Silberman <orisil1809@gmail.com>
2021-12-21 21:33:23 +0200

    Added teleport logic to firstScene

---
+++ Assets/Scripts/LocomotionController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class LocomotionController : MonoBehaviour
+{
+    public XRController leftTelporterRay;
+    public XRController rightTelporterRay;
+    public InputHelpers.Button teleportActivateBtn;
+    public float activationThreshold = 0.1f;
+
+    public bool checkIfActivated(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, teleportActivateBtn, out bool isActivated, activationThreshold);
+        return isActivated;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (leftTelporterRay)
+        {
+            leftTelporterRay.gameObject.SetActive(checkIfActivated(leftTelporterRay));
+        }
+        if (rightTelporterRay)
+        {
+            rightTelporterRay.gameObject.SetActive(checkIfActivated(rightTelporterRay));
+        }
+
+    }
+}
