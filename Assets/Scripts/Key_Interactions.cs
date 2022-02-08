49d6366ec693e3cb2cdf895bf2091a303aa42c93
Ori Silberman <orisil1809@gmail.com>
2022-02-08 17:14:50 +0200

    Fixed key when opening door

--- Assets/Scripts/Key_Interactions.cs
+++ Assets/Scripts/Key_Interactions.cs
@@ -7,10 +7,11 @@ public class Key_Interactions : MonoBehaviour
 {
     public GameObject key_hole;
     public Animator door_animator;
+    public GameObject spare_key;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -19,18 +20,44 @@ public class Key_Interactions : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter(Collision collision)
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.Equals(key_hole))
+    //    {
+    //        door_animator.SetTrigger("OpenDoor");
+    //        //XRGrabInteractable.Destroy(this);
+    //        Destroy(this.GetComponent<XRGrabInteractable>());
+    //        this.gameObject.GetComponent<Rigidbody>().useGravity = false;
+    //        this.transform.position = new Vector3(-14.25f, 14.89f, 0.54f);
+    //        this.transform.rotation = Quaternion.Euler(new Vector3(90f, 0f, -90f));
+    //        this.transform.SetParent(key_hole.transform);
+
+    //    }
+    //}
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.Equals(key_hole))
+        if (other.gameObject.Equals(key_hole))
         {
+            this.gameObject.SetActive(false);
+            spare_key.SetActive(true);
             door_animator.SetTrigger("OpenDoor");
             //XRGrabInteractable.Destroy(this);
-            Destroy(this.GetComponent<XRGrabInteractable>());
-            this.gameObject.GetComponent<Rigidbody>().useGravity = false;
-            this.transform.position = new Vector3(-14.25f, 14.89f, 0.54f);
-            this.transform.rotation = Quaternion.Euler(new Vector3(90f, 0f, -90f));
-            this.transform.SetParent(key_hole.transform);
+            //Destroy(this.GetComponent<XRGrabInteractable>());
+            //this.gameObject.GetComponent<Rigidbody>().useGravity = false;
+            //this.transform.position = new Vector3(-14.25f, 14.89f, 0.54f);
+            //this.transform.rotation = Quaternion.Euler(new Vector3(90f, 0f, -90f));
+            //this.transform.SetParent(key_hole.transform);
 
         }
+    }
+
+    public void EnableIsTrigger()
+    {
+        this.gameObject.GetComponent<MeshCollider>().isTrigger = true;
+    }
+
+    public void DisableIsTrigger()
+    {
+        this.gameObject.GetComponent<MeshCollider>().isTrigger = false;
     }
 }
