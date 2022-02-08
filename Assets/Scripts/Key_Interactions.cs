22e4851a3481a05c437521738cca7ebe2b664046
Ori Silberman <orisil1809@gmail.com>
2022-02-08 16:00:52 +0200

    Added first room key interactions and success with opening chest

---
+++ Assets/Scripts/Key_Interactions.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Key_Interactions : MonoBehaviour
+{
+    public GameObject key_hole;
+    public Animator door_animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.Equals(key_hole))
+        {
+            door_animator.SetTrigger("OpenDoor");
+            //XRGrabInteractable.Destroy(this);
+            Destroy(this.GetComponent<XRGrabInteractable>());
+            this.gameObject.GetComponent<Rigidbody>().useGravity = false;
+            this.transform.position = new Vector3(-14.25f, 14.89f, 0.54f);
+            this.transform.rotation = Quaternion.Euler(new Vector3(90f, 0f, -90f));
+            this.transform.SetParent(key_hole.transform);
+
+        }
+    }
+}
